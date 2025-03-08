ec0d5015e720d60eab2d57169096987f253a2ffb
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-08 23:08:54 +0300

    Added logout functionallity

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,6 +5,7 @@ import { getCollection } from "@/lib/db";
 import { LoginFormSchema, RegisterFormSchema } from "@/lib/rules";
 import { redirect } from "next/navigation";
 import { createSession } from "@/lib/sessions";
+import { cookies } from "next/headers";
 
 export async function register(state, formData) {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -50,7 +51,8 @@ export async function register(state, formData) {
   });
 
   // Create a session
-  await createSession(results.insertedId.toString());
+  await createSession(results.insertedId);
+
   // Redirect
   redirect("/dashboard");
 }
@@ -77,18 +79,24 @@ export async function login(state, formData) {
   const userCollection = await getCollection("users");
   if (!userCollection) return { errors: { email: "Server error!" } };
 
-  const existingUser = await userCollection.findOne({email})
+  const existingUser = await userCollection.findOne({ email });
   if (!existingUser) return { errors: { email: "Invalid credentials." } };
 
   // Check password
-  const matchedPassword = await bcrypt.compare(password, existingUser.password)
+  const matchedPassword = await bcrypt.compare(password, existingUser.password);
   if (!matchedPassword) return { errors: { email: "Invalid credentials." } };
 
   // Create a session
-  await createSession(existingUser._id.toString())
+  await createSession(existingUser._id.toString());
 
   console.log(existingUser);
-  
+
   // Redirect
-  redirect('/dashboard')
+  redirect("/dashboard");
+}
+
+export async function logout() {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+  redirect("/");
 }
