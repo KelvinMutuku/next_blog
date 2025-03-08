5dcb393eb9d2b6f9dca893f49d27578a6299bb02
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-08 15:38:44 +0300

    Updated auth.js and page.jsx

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,22 @@
 "use server";
-export async function register(state, formData) { //adding the async function and "use server" fixes this error :Server Actions must be async functions
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    const email = formData.get("email");
-    const password = formData.get("password");
-    const confirmPassword = formData.get("confirmPassword");
 
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
+import { RegisterFormSchema } from "@/lib/rules";
+
+export async function register(state, formData) {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const validatedFields = RegisterFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      email: formData.get("email"),
+    };
+  }
+
+  console.log(validatedFields);
 }
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,30 +1,58 @@
-"use client"  // Fixes this error: "You're importing a component that needs `useActionState`. This React hook only works in a client component. To fix, mark the file (or its parent) with the `"use client"` directive.
-import {useActionState} from 'react'
+"use client";
+
+import { useActionState } from "react";
+import { register } from "@/actions/auth";
 import Link from "next/link";
-import {register} from '@/actions/auth';
+
 export default function Register() {
-    const [state, action, isPending] = useActionState(register, undefined);
-    return (
-        <div className="container w-1/2">
-            <h1 className="title">Register</h1>
-            <form action="" className="space-y-4">
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email"/>
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password"/>
-                </div>
-                <div>
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" name="confirmPassword"/>
-                </div>
-                <div className="flex items-end gap-4">
-                    <button  disabled={isPending} className="btn-primary">{isPending ? "Loading..." : "Register"}</button>
-                    <Link href="/" className="text-link">or login here</Link>
-                </div>
-            </form>
+  const [state, action, isPending] = useActionState(register, undefined);
+
+  return (
+    <div className="container w-1/2">
+      <h1 className="title">Register</h1>
+
+      <form action={action} className="space-y-4">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" defaultValue={state?.email} />
+          {state?.errors?.email && (
+            <p className="error">{state.errors.email}</p>
+          )}
         </div>
-    );
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+          {state?.errors?.password && (
+            <div className="error">
+              <p>Password must:</p>
+              <ul className="list-disc list-inside ml-4">
+                {state.errors.password.map((err) => (
+                  <li key={err}>{err}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input type="password" name="confirmPassword" />
+          {state?.errors?.confirmPassword && (
+            <p className="error">{state.errors.confirmPassword}</p>
+          )}
+        </div>
+
+        <div className="flex items-end gap-4">
+          <button disabled={isPending} className="btn-primary">
+            {isPending ? "Loading..." : "Register"}
+          </button>
+
+          <Link href="/" className="text-link">
+            or login here
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
