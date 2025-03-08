83c366f45b212032888d8d85b421fbf5ed46d511
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-08 12:35:04 +0300

    Updated layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,15 @@
 import "./globals.css";
-
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <header>
-          <nav>nav</nav>
+          <nav>
+            <Link className="nav-link" href="/">Home</Link>
+            <Link className="nav-link" href="/dashboard">Dashboard</Link>
+          </nav>
         </header>
 
         <main>{children}</main>
